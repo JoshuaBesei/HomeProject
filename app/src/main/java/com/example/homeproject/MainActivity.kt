f8985a09388dbbe73b7ff5666f0093ace461d26d
Aldric-Choi-s3761165 <s3761165@student.rmit.edu.au>
2021-04-03 17:52:21 +1100

    Added the calendar in the add tasks fragment and added calendar mathods in the MainActivity.kt

--- app/src/main/java/com/example/homeproject/MainActivity.kt
+++ app/src/main/java/com/example/homeproject/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.homeproject
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
-import android.widget.RadioGroup
-import android.widget.ToggleButton
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.example.homeproject.R.id.High
 import com.example.homeproject.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    var button_date: Button? = null
+    var textview_date: TextView? = null
+    var cal = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val homeFragment = HomeFragment()
         val account = AccountFragment()
-        val leaderboards = LeaderBoardsFragment()
+        val leaderBoards = LeaderBoardsFragment()
         val completedTasks = CompletedTasksFragment()
         val addTasks = AddTasksFragment()
         var checked = "High"
@@ -26,34 +34,52 @@ class MainActivity : AppCompatActivity() {
         makeCurrentFragment(homeFragment)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val toggleButtonHigh = findViewById<ToggleButton>(R.id.High)
-        val toggleButtonLow = findViewById<ToggleButton>(R.id.Low)
+        val buttonHigh = findViewById<Button>(R.id.High)
+        val buttonLow = findViewById<Button>(R.id.Low)
 
         bottomNavigation.setOnNavigationItemSelectedListener {
             when (it.itemId){
                 R.id.home -> makeCurrentFragment(homeFragment)
                 R.id.account -> makeCurrentFragment(account)
-                R.id.leaderboards -> makeCurrentFragment(leaderboards)
+                R.id.leaderboards -> makeCurrentFragment(leaderBoards)
                 R.id.completedTasks -> makeCurrentFragment(completedTasks)
                 R.id.addTasks -> makeCurrentFragment(addTasks)
             }
             true
         }
 
-        /*
-        toggleButtonHigh.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                checked = "High"
-            }
-        }*/
 
-        /*
-        toggleButtonLow.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                checked = "Low"
-            }
-        }*/
 
+        textview_date = findViewById<TextView>(R.id.text_view_date_1)
+        button_date = findViewById<Button>(R.id.button_date_1)
+
+        textview_date!!.text = "--/--/----"
+
+        // create an OnDateSetListener
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+
+        // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
+        button_date!!.setOnClickListener {
+            DatePickerDialog(this@MainActivity,
+                dateSetListener,
+                // set DatePickerDialog to point to today's date when it loads up
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        textview_date!!.text = sdf.format(cal.getTime())
     }
 
     private fun makeCurrentFragment(fragment: Fragment) =
